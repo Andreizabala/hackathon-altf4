82b449f006d6783a5381a4d2981f32cff51e3496
glendell03 <dellbringino@gmail.com>
2020-11-27 12:19:19 +0800

    Added Firestore

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,8 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const app = require("express")();
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.use(cors())
+
+
+exports.api = functions.https.onRequest(app);
